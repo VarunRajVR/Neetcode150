36e08655e85c0ad201dd77dc69950f8c673043c5
Varun Raj V R <varunrajvr2016@gmail.com>
2025-09-02 21:38:15 -0700

    LL

--- Linked_list.py
+++ Linked_list.py
@@ -109,30 +109,7 @@ class Solution(object):
             first = temp1
             second = temp2
         
-#remove nth node from end of list.# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        dummy = ListNode(0, head)
-        left = dummy
-        right = head
-        for i in range(n):
-            if right:
-                right = right.next
-                n-=1
-        while right:
-            left = left.next
-            right = right.next
-        left.next = left.next.next
-        return dummy.next
+
         
 #remove nth node from end of list.
 # Definition for singly-linked list.
@@ -158,3 +135,178 @@ class Solution(object):
             right = right.next
         left.next = left.next.next
         return dummy.next
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+#copy list with random pointer.
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        oldToCopy = {None: None}
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next]
+            copy.random = oldToCopy[cur.random]
+            cur = cur.next
+        return oldToCopy[head]
+    
+#add two numbers.
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode()
+        cur = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update ptrs
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
+
+#find the duplicate number.
+# floyd's tortoise and hare (cycle detection) algorithm
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+
+#merge k sorted lists.
+        # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        # Create a dummy node before head for easier handling
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            # Find the kth node from groupPrev
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break  # not enough nodes left to form a group
+
+            groupNext = kth.next  # node after the kth node
+
+            # Reverse group
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            # After reversing, adjust pointers
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+
+        return dummy.next
+
+    def getKth(self, curr, k):
+        # Move k steps ahead to find the kth node
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr     
+
+
+#reverse nodes in k-group.
+        # Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        # Create a dummy node before head for easier handling
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            # Find the kth node from groupPrev
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break  # not enough nodes left to form a group
+
+            groupNext = kth.next  # node after the kth node
+
+            # Reverse group
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            # After reversing, adjust pointers
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+
+        return dummy.next
+
+    def getKth(self, curr, k):
+        # Move k steps ahead to find the kth node
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
+
+
--- binary_search.py
+++ binary_search.py
@@ -8,11 +8,10 @@ class Solution(object):
         n = len(nums)
         low = 0
         high = n-1
-        flag = False
+
         while low<=high:
             mid = (low+high)//2
             if nums[mid] == target:
-                flag = True
                 return mid
             elif nums[mid] < target:
                 low = mid+1
@@ -42,6 +41,7 @@ class Solution(object):
 
         if not (top <= bot):
             return False
+        #binary search the column
         row = (top + bot) // 2
         l, r = 0, COLS - 1
         while l <= r:
@@ -54,7 +54,29 @@ class Solution(object):
                 return True
         return False
         
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        l, r = 1, max(piles)
+        res = r
 
+        while l <= r:
+            k = (l + r) // 2
+
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(float(p) / k)
+            if totalTime <= h:
+                res = k
+                r = k - 1
+            else:
+                l = k + 1
+        return res
+    
 #find minimum in rotated sorted array
 class Solution(object):
     def findMin(self, nums):
@@ -110,3 +132,88 @@ class Solution(object):
                     l = mid + 1
 
         return -1
+
+class TimeMap(object):
+
+    def __init__(self):
+        self.store= {}
+
+    def set(self, key, value, timestamp):
+        """
+        :type key: str
+        :type value: str
+        :type timestamp: int
+        :rtype: None
+        """
+        if key not in self.store:
+            self.store[key]= []
+        self.store[key].append((value, timestamp))
+
+    def get(self, key, timestamp):
+        """
+        :type key: str
+        :type timestamp: int
+        :rtype: str
+        """
+        res = ""
+        values = self.store.get(key, [])
+        l, r= 0, len(values)-1
+        while l<=r:
+            mid = (l+r)//2
+            if values[mid][1] <=timestamp:
+                res = values[mid][0]
+                l = mid+1
+            else:
+                r =mid-1
+        return res
+
+
+
+
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+       # Ensure nums1 is the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        m, n = len(nums1), len(nums2)
+        total = m + n
+        half = total // 2
+
+        left, right = 0, m
+        while left <= right:
+            i = (left + right) // 2  # partition in nums1
+            j = half - i             # partition in nums2
+
+            # nums1 left and right values
+            left1 = nums1[i-1] if i > 0 else float("-inf")
+            right1 = nums1[i] if i < m else float("inf")
+
+            # nums2 left and right values
+            left2 = nums2[j-1] if j > 0 else float("-inf")
+            right2 = nums2[j] if j < n else float("inf")
+
+            # valid partition
+            if left1 <= right2 and left2 <= right1:
+                # odd total length
+                if total % 2:
+                    return float(min(right1, right2))
+                # even total length
+                return (max(left1, left2) + min(right1, right2)) / 2.0
+            elif left1 > right2:
+                right = i - 1
+            else:
+                left = i + 1
